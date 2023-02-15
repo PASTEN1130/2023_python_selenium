25d212b48e93c3cefa3eb8117d7366445ec9686f
Brenda Pasten <pastenbren@gmail.com>
2023-02-14 20:34:46 -0600

    Ejercicio 3 Brenda Hernandez

---
+++ python_03.py
@@ -0,0 +1,18 @@
+nombre=input("¿cual es tu nombre?")#Pregunte al usuario que nombre tiene,  almacena el valor en una variable.
+print(nombre)
+edad = input("¿Cual es tu edad?")#Pregunta al usuario que edad tiene, almacena el valor en una variable.
+print(edad)
+ciudad=input("¿Cual es tu ciudad?")#Pregunta al usuario cual es su ciudad, almacena el valor en una variable.
+print(ciudad)
+estado_civil=input("¿Estas casado?")#Pregunta al usuario si está casado, almacena el valor en una variable.
+print(estado_civil)
+num_1=input("¿Elige un numero entre 1 - 100")#Pídele al usuario un número entre 1-100
+num_1=int(num_1)
+print(num_1)
+num_2=input("¿Elige un numero entre 100 - 200")#Pídele al usuario un número entre 100-200
+print(num_2)
+num_2=int(num_2)
+resultado=(num_1/num_2)
+print(f"Mi nombre es {nombre}, mi edad es {edad} años, soy de la ciudad de {ciudad}, y mi resultado es {num_1/num_2}")#Imprime la  información en pantalla
+
+
