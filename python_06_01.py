28a0a470d6f5157de559572f2747f5f1fa7646c4
Brenda Pasten <pastenbren@gmail.com>
2023-03-27 09:50:11 -0600

    ejercicios

--- python_06_01.py
+++ python_06_01.py
@@ -6,13 +6,16 @@ dado que se debe ser mayor a 14 años y tener una altura no menor de 1,62.
  El script debe ser capaz de informar si puede o no subirse y en el caso de que no,
  porque razon (Si por edad, por tamaño u ambas)
  """
-edad = input ("Cual es tu edad: "), int
-altura = input ("Cual es tu altura: "), float
-edad =
+edad_persona= input ("Cual es tu edad: ")
+edad=int(edad_persona)
+altura_persona = input ("Cual es tu altura: ")
+edad=float(altura_persona)
 
-if edad >= 14 and altura >= 1.62:
+
+if edad_persona >= 14 and altura_persona >= 1.62:
+
     print (f"Puedes subir a la montaña Push Pull {edad} ,{altura}")
 
 else:
 
-    print("No puedes subir a la montaña Push pull")
+    print(f"No puedes subir a la montaña Push pull porque tu {edad} y tu {altura} no esta permitida ")
