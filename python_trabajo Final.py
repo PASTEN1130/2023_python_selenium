47613e945eee0df98fcdb2c38a114d1e003df169
Brenda Pasten <pastenbren@gmail.com>
2023-03-27 12:07:35 -0600

    ejercicios

--- python_trabajo Final.py
+++ python_trabajo Final.py
@@ -1,5 +1,46 @@
-VALID_RAZA = ("TERRAN", "ZERG", "PROTOSS")
+def imprimir_header(header: str):
+    print(f"{40 * '='} {header} {40 * '='}")
+##########################################################################################
+# GENERAR PARTIDAS ALEATORIAS
+##########################################################################################
+from random import choice
 
+JUGADORES = []
+print(jugador)
+
+partida_A = []
+
+jugador = choice(jugador)
+print(jugador)
+partida_A.append(jugador)
+jugadores.remove(jugador)
+print("jugadores restantes:",jugador)
+
+
+
+def imprimir_header(header: str):
+    print(f"{40 * '='} {header} {40 * '='}")
+##########################################################################################
+# IMPORTAR ARCHIVO
+##########################################################################################
+import json
+
+JUGADORES = {
+    "JUGADORES": [
+        {"nombre_jugador", "mail", "estado", "raza"}
+    ]
+}
+with open("directory.json", "w") as file:
+    json.dump(JUGADORES, file)
+
+with open("directory.json") as file:
+    data = json.load(file)
+    print(type(data))
+    for contact in data["contacts"]:
+        print(JUGADORES)
+
+
+raza = ["TERRAN", "ZERG", "PROTOSS"]
 def imprimir_header(header: str):
     print(f"{40 * '='} {header} {40 * '='}")
 
@@ -14,7 +55,7 @@ def alta():
     estado=input("Estado del Jugador:")
     jugador = jugador(nombre_jugador, mail, estado, raza[str, str, str])
     JUGADORES.append(jugador)
-    print(f"Alumno registrado: {jugador}")
+    print(f"Jugador Registrado: {jugador}")
 
 ##########################################################################################
 # CONTROL PRINCIPAL
@@ -25,7 +66,6 @@ MENU = {
     "Alta": alta,
 
 }
-
 OPTIONS = ' | '.join(MENU.keys())  # alta
 
 while True:
