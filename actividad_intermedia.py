d2b6ad2631c5f382109e161dce46c691a4fa9bd0
Brenda Pasten <pastenbren@gmail.com>
2023-03-27 09:38:53 -0600

    ejercicios

--- actividad_intermedia.py
+++ actividad_intermedia.py
@@ -9,39 +9,90 @@ El sistema debe permitir mostrar TODOS los Cursos existentes, como también la p
 """
 def add_course(lst: list):
     print("=" * 80)
-    print("Agregar nuevo curso:")
+    print("AGREGAR CURSO")
     print("=" * 80)
-    nombre_cur = input("Ingrese Nombre del curso:")
-    can_alumnos = input("Ingrese cantidad de alumnos:")
-    estado = input("Ingrese el estado del curso:")
-    can_clases = input("Ingrese la cantidad de clases:")
+    name = input("NOMBRE DEL CURSO?")
+    total_student = input("TOTAL DE ALUMNOS?")
+    total_sessions = input("TOTAL DE CLASES?")
+    status = input(f"Status? {' | '.join(VALID_STATUS) }")
+    if status in VALID_STATUS:
+        course = {
+            "name": name,
+            "totalStudent": total_student,
+            "totalSessions": total_sessions,
+            "status": status
+        }
+        lst.append(course)
+        print("Curso agregado")
+        show_course(course)
+    else:
+        print(f"Estado no valido: {status}")
 
-    nueva_entrada = {
-        "nombre_cur": nombre_cur,
-        "can_alumnos": can_alumnos,
-        "estado": estado,
-        "can_clases": can_clases,
-    }
-    lst.append(nueva_entrada)
-    print(lst)
 
-def search(lst: list):
-    pass
+def search_course(lst: list):
+    print("=" * 80)
+    print("BUSCAR CURSO")
+    print("=" * 80)
+    target = input("Nombre?")
+    found = [course for course in lst if course["name"] == target]
+    if found and len(found) > 0:
+        print(f"HAZ ENCONTRADO EL CURSO!")
+        show_course(found[0])
+        update = input(f"QUIERES ACTUALIZAR? (YES|NO)")
+        if update == "YES":
+            update_course(found[0])
+    else:
+        print("Curso no encontrado")
 
-def modify(lst: list):
-    pass
 
-libreria = {}
+def show_courses(lst: list):
 
-cursos = []
+    names = [course["name"] for course in lst]
+    print(f"CURSOS: {names}")
+    show_details = input("DESEAS VER LOS DETALLES ? (YES|NO)")
+    if show_details == "YES":
+        for course in lst:
+            show_course(course)
+
+
+def update_course(course: dict):
+    print("=" * 80)
+    print("ACTUALIZAR CURSO")
+    print("=" * 80)
+    status = input(f"Status? {' | '.join(VALID_STATUS) }")
+    if status in VALID_STATUS:
+        course["status"] = status
+        print("CURSO ACTUALIZADO")
+        show_course(course)
+    else:
+        print(f"Estado no valido: {status}")
+
+
+def show_course(course: dict):
+
+    print(f"\t--->[{course['name']}]: Status = {course['status']}")
+
+
+def close(lst: list):
+
+    print("CLOSING...")
+    print(f"Cursos: {lst}")
+    exit(0)
+
+
+VALID_STATUS = ("No Activo", "Activo", "Suspendido")
+COURSES = []
+MENU_OPTIONS = {
+    "ADD": add_course,
+    "UPDATE": search_course,
+    "SHOW_ALL": show_courses,
+    "CLOSE": close
+}
 
 while True:
-    action = input("Que accion deseas hacer? (ADD | SEARCH | MODIFY)")
-    if action == "ADD":
-        add_course(cursos)
-    elif action == "SEARCH":
-        search(cursos)
-     elif action == "UPDATE":
-        modify(cursos)
-    else:
-        print(f"Opcion invalida: {action}")
+    for option, method in MENU_OPTIONS.items():
+        action = input(f"Que accion deases realizar: {' | '.join(MENU_OPTIONS.keys())}\n")
+        if action in MENU_OPTIONS.keys():
+            MENU_OPTIONS[action](COURSES)
+        else:
+            print(f"Action not supported: {action}")
